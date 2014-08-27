fdb3e5b7d94c1276fc6ebcd8160ce4720be98049
Milos Stojanovic <milos92s@yahoo.com>
2014-08-27 19:38:40 +0200

    Refactoring

--- HSMSWebService/IHSMSService.cs
+++ HSMSWebService/IHSMSService.cs
@@ -49,4 +49,72 @@ namespace HSMSWebService
             set { stringValue = value; }
         }
     }
+
+    [DataContract]
+    public class HSMS
+    {
+        private int id;
+        private string desc;
+        private string number;
+        private string price;
+        private string organisation;
+        private int priority;
+
+        [DataMember]
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string Desc
+        {
+            get { return desc; }
+            set { desc = value; }
+        }
+
+        [DataMember]
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        [DataMember]
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        [DataMember]
+        public string Organisation
+        {
+            get { return organisation; }
+            set { organisation = value; }
+        }
+
+        [DataMember]
+        public int Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+
+        public HSMS()
+        {
+
+        }
+
+        public HSMS(int id, string desc, string number, string price, string organisation, int priority)
+        {
+            this.id = id;
+            this.desc = desc;
+            this.number = number;
+            this.price = price;
+            this.organisation = organisation;
+            this.priority = priority;
+        }
+    }
 }
