e5e56a1c118144ccad1c69e214ef9d996ae53d05
Milos Stojanovic <milos92s@yahoo.com>
2014-08-28 14:20:23 +0200

    Refactoring.

--- HSMSWebService/HSMSService.svc.cs
+++ HSMSWebService/HSMSService.svc.cs
@@ -11,26 +11,8 @@ namespace HSMSWebService
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     public class HSMSService : IHSMSService
     {
-
+        
         private HSMSDatabase database = new HSMSDatabase();
-
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
 
         public HSMS[] getAllActions()
         {
